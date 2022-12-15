09a2ba588cec653e002a5f907a17a4765f9cd847
Justin Chang <justinchangmusic@gmail.com>
2022-12-15 15:13:07 -0500

    Add rest of notebooks

--- jupyter_helpers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .github_paperspace import create_github_sshkey_on_machine
-from .gpu_memory import clean_memory
-from .gpu_training import ToDeviceCallback, device, safely_train_with_gpu
-from .kaggle import create_submission, download_data
